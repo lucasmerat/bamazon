ef529f65623a643b030befdf721320fe1ffc5314
Lucas Merat <lucas.merat@gmail.com>
2019-03-12 14:59:29 -0500

    Added validation and looping for bamazon customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,100 +1,128 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "OiTuimMbWiw10!",
-    database: "bamazon"
-  });
-  
-  // connect to the mysql server and sql database
-  // connection.connect(function(err) {
-  //   if (err) throw err;
-  //   // run the start function after the connection is made to prompt the user
-  //   console.log("Connected!")
-  // })
+  host: "localhost",
 
-  function readItems() {
-    connection.query("select * from products", function(err, results) {
-      if (err) throw err;
-      console.log('Available items:')
-      for(let i=0;i<results.length;i++){
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "OiTuimMbWiw10!",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  readItems();
+});
+
+function readItems() {
+  connection.query("select * from products", function(err, results) {
+    if (err) throw err;
+    console.log("Available items:");
+    for (let i = 0; i < results.length; i++) {
       console.log(`
 -------------      
 ID: ${results[i].item_id}
 Product name: ${results[i].product_name}
 Price: $${results[i].price}
 -------------
-      `)
-     // connection.end();
-      }
-      console.log("Above are all available items. Please enter the ID number of the item you would like to bid on")
-    });
-  }
-  
-  function checkQuantity(id, orderQuantity){
-    console.log("the id is " + id)
-  
+      `);
+      // connection.end();
+    }
+    start();
+  });
 }
 
-  readItems();
-
-  inquirer.prompt([
+function start() {
+  inquirer
+    .prompt([
       {
-        type:"input",
-        name:"item",
-        message:"Type the ID of the item you would like to buy"
+        type: "input",
+        name: "item",
+        message: "Type the ID of the item you would like to buy",
+        validate: function(value) {
+          if (isNaN(value)) {
+            return false;
+          } else {
+            return true;
+          }
+        }
       },
       {
-        type:"input",
-        name:"quantity",
-        message:"How many units of the product do you want to purchase?"
+        type: "input",
+        name: "quantity",
+        message: "How many units of the product do you want to purchase?",
+        validate: function(value) {
+          if (isNaN(value)) {
+            return false;
+          } else {
+            return true;
+          }
+        }
       }
-  ]).then(function(order){
+    ])
+    .then(function(order) {
       let orderQuantity = parseInt(order.quantity);
       let id = Number(order.item);
-      connection.query("SELECT stock_quantity, product_sales, price FROM products WHERE item_id = ?", id, function(err, results){
-        if(err) throw err;
-        let price = results[0].price
-        console.log(price)
-        if (results[0].stock_quantity > orderQuantity){
-            let newQuantity = results[0].stock_quantity - orderQuantity
+      connection.query(
+        "SELECT stock_quantity, product_sales, price FROM products WHERE item_id = ?",
+        id,
+        function(err, results) {
+          if (err) throw err;
+          let price = results[0].price;
+          if (results[0].stock_quantity > orderQuantity) {
+            let newQuantity = results[0].stock_quantity - orderQuantity;
             let pricePaid = orderQuantity * price;
             let previousSales = results[0].product_sales;
-            updateDatabase(newQuantity, pricePaid, previousSales, id)
-            console.log(`Success, you purchased ${orderQuantity} items for $${pricePaid}`)
-            // connection.end();
-        } else {
-          console.log('Insufficient quantity of product, please try again')
-          // connection.end();
-        }
-    })
-  })
-
-  function updateDatabase(newQuantity, pricePaid, previousSales, id){
-    let newSales = previousSales + pricePaid;
-    console.log(newSales)
-    connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [
-          {
-            stock_quantity: newQuantity,
-            product_sales: newSales
-          },
-          {
-            item_id: id
+            updateDatabase(newQuantity, pricePaid, previousSales, id);
+            console.log(
+              `Success, you purchased ${orderQuantity} items for $${pricePaid}`
+            );
+            inquirer
+              .prompt([
+                {
+                  type: "list",
+                  name: "another",
+                  message: "Do you want to purchase another product?",
+                  choices: ["yes", "no"]
+                }
+              ])
+              .then(function(answer) {
+                if (answer.another === "yes") {
+                  start();
+                } else {
+                  connection.end();
+                }
+              });
+          } else {
+            console.log("Insufficient quantity of product, please try again");
+            start();
           }
-        ],
-        function(err) {
-          if (err) throw err;
         }
       );
-  }
+    });
+}
+
+function updateDatabase(newQuantity, pricePaid, previousSales, id) {
+  let newSales = previousSales + pricePaid;
+  connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: newQuantity,
+        product_sales: newSales
+      },
+      {
+        item_id: id
+      }
+    ],
+    function(err) {
+      if (err) throw err;
+    }
+  );
+}
